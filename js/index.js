2c99ff7afacdbe7a73deb885a01914f9cf209859
clafauriegalvis <101742801+clafauriegalvis@users.noreply.github.com>
2022-04-10 19:06:35 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,4 @@
 
-
-
-
 const currencyEl_one = document.getElementById('currency-one');
 const currencyEl_two = document.getElementById('currency-two');
 const amountEl_one = document.getElementById('amount-one');
